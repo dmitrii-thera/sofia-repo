afb0ad5517b9693527458f086b18458e12e095d6
dmitrii-thera <159258410+dmitrii-thera@users.noreply.github.com>
2025-07-26 00:21:53 +0300

    s02t03u07

---
+++ src/s02t03u07/main.cpp
@@ -0,0 +1,21 @@
+// https://practicum.yandex.ru/trainer/cpp/lesson/43d4e584-9156-4521-b091-a8bbc0a69c03/task/1015e3ea-8864-4f7b-bd0f-b3ab628e91d2/
+enum class TaskStatus {
+    NEW, IN_PROGRESS, TESTING, DONE
+    // 0,   1            2        3
+}
+
+// static_cast<TaskStatus>(static_cast<int>(status) + 1);
+
+using TaskInfo = map<TaskStatus, int>;
+class TeamTask {
+    
+    
+// Example 2; Sasha NEW = 3; task_count = 10;
+for (auto& [status, count]: person_tasks) {
+    // status = NEW; count = 3; task_count = 10
+
+private:
+    map<string, TaskInfo> tasks_info_;
+};
+
+    
